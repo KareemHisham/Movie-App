cc908406c5295bd24fb4aefff58741b3506b8c43
unknown <kareem.hisham.ahmed@gmail.com>
2020-02-08 22:48:43 +0200

    Updates

---
+++ precache-manifest.8c83d47e802397122e2716f758e205c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "a715aee27ff2c2360d83",
+    "url": "/movie-app/static/js/main.a715aee2.chunk.js"
+  },
+  {
+    "revision": "94369a59a5b524149e42",
+    "url": "/movie-app/static/js/1.94369a59.chunk.js"
+  },
+  {
+    "revision": "a715aee27ff2c2360d83",
+    "url": "/movie-app/static/css/main.278425fe.chunk.css"
+  },
+  {
+    "revision": "2e621949b2359f5a6dc536e24e2f8ef5",
+    "url": "/movie-app/index.html"
+  }
+];
